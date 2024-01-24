4d566e372d9e299d5542e63b423d4a6351e757cf
achyarrbagus <achyarbagus@example.com>
2024-01-24 17:12:22 +0700

    update footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,7 +97,48 @@ export default function RootLayout({
           </div>
         </nav>
         {children}
-
+        <footer className="footer">
+          <div className="container">
+            <div className="d-flex align-items-center">
+              <a href="/">
+                <img src="/assets/img/logo-white.png" className="logo" alt="" />
+              </a>
+              <img src="/assets/img/logo-atensi.png" className="logo-atensi ms-auto" alt="" />
+            </div>
+            <div className="row mt-4">
+              <div className="col-md-8 text-white">
+                <h4 className="fs-14">PT Cepat Sehat Indonesia</h4>
+                <p>
+                  Jalan Peternakan No. 13, Kel. Tengah, Kec. Kramat jati, Kota Adm. Jakarta Timur, Provinsi DKI Jakarta 13510{" "}
+                  <br />
+                  sehatcepat.com
+                </p>
+              </div>
+              <div className="col-md-12">
+                <div className="d-flex align-items-center">
+                  <a
+                    href="https://api.whatsapp.com/send/?phone=6282211189009&text&type=phone_number&app_absent=0"
+                    className="phone text-white fs-14 d-flex align-items-center"
+                  >
+                    <i className="mdi mdi-phone me-2 fs-24"></i> 0822 1118 9009
+                  </a>
+                  <div className="sosmed list-inline ms-auto">
+                    <a href="#" className="list-inline-item text-white">
+                      <i className="mdi mdi-web fs-24"></i>
+                    </a>
+                    <a href="https://www.facebook.com/klinikcepatsehat" className="list-inline-item text-white">
+                      <i className="mdi mdi-facebook fs-24"></i>
+                    </a>
+                    <a href="https://www.instagram.com/sehatcepat.mobi/ " className="list-inline-item text-white">
+                      <i className="mdi mdi-instagram fs-24"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="footer-bottom">Copyright © 2024 Cepat Sehat. All Rights Reserved.</div>
+        </footer>
         <div
           className="btn-float"
           data-bs-toggle="tooltip"
