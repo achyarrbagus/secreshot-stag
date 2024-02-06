65b907c437102cd60fbc41f6858e1a44ad9b4bb9
achyarrbagus <achyarbagus@example.com>
2024-02-06 22:00:10 +0700

    update tracking gtm

--- src/app/components/layout-wrapper.jsx
+++ src/app/components/layout-wrapper.jsx
@@ -29,6 +29,18 @@ function LayoutWrapper({ children }) {
       r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
       a.appendChild(r);
     })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+
+    // gtm tracking
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, "script", "dataLayer", "GTM-PFX6ZWQ4");
   }, []);
 
   return (
@@ -151,6 +163,12 @@ function LayoutWrapper({ children }) {
           </a>
         </div>
       </div>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-PFX6ZWQ4"
+        height="0"
+        width="0"
+        style={{ display: "display:none;visibility:hidden" }}
+      ></iframe>
 
       <footer className="footer">
         <div className="container">
