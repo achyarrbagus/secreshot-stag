f046b4442cbf403a5e3f8d307ea26a226310badd
achyarrbagus <achyarbagus@example.com>
2024-02-09 14:16:01 +0700

    update wa wording

--- pages/anti-aging/index.tsx
+++ pages/anti-aging/index.tsx
@@ -20,21 +20,13 @@ const Index: NextPage = () => {
         return alert("please fill form correcly");
       }
       const wardingWa = encodeURIComponent(`
-Hello Admin,
-I want to book service with
-following information :
+Hello CepatSehat.com by Cepat Sehat Clinic, i want a consultation 
+
 
 Name : ${name.value}
-Address : ${address.value}
-Service Name : ${service.value}
-----------------------------------------------------------------------     
-Halo Admin,
-Saya ingin melakukan booking service dengan keterangan 
-berikut:
+Address : ${address.value} 
+Service : ${service.value}`);
 
-Nama : ${name.value}
-Alamat : ${address.value} 
-Service Name : ${service.value}`);
       let url = `https://api.whatsapp.com/send/?phone=6282211189009&text=${wardingWa}&type=phone_number&app_absent=0`;
       window.location.href = url;
 
