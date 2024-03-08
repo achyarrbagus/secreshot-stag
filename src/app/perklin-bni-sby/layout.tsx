ad0788cd6a580ded0f719d5b2e606b896a748fd0
erwin dwi s <erwin@waki.mobi>
2024-03-08 10:40:19 +0700

    layout

---
+++ src/app/perklin-bni-sby/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+// import "../../../src/app/global.css";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+// import { Providers } from "../../";
+// import LayoutWrapper from "../app/components/layout-wrapper";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: '/favicon.svg',
+  },
+  title: "Cepat Sehat",
+  description: "We deliver personalized and compassionate healthcare to your home through our experienced professionals",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+    <Head>
+      <head></head>
+    </Head>
+      <body className={inter.className}>
+      
+          {children}
+        
+      </body>
+    </html>
+  );
+}
