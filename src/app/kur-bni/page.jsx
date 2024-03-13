9f0b03a7e27543f711eefd004d5a1fa7d0954888
erwin dwi s <erwin@waki.mobi>
2024-03-13 13:22:12 +0700

    feat: add filter date

--- src/app/kur-bni/page.jsx
+++ src/app/kur-bni/page.jsx
@@ -1,6 +1,9 @@
 "use client"
 import { useEffect, useState } from 'react';
 import DataTable from 'react-data-table-component';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import {format} from "date-fns";
 import * as XLSX from 'xlsx';
 
 const columns = [
@@ -51,25 +54,36 @@ const dataKurBni = () => {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState(new Date());
+  
   useEffect(() => {
     const fetchDiscount = async () => {
       try {
-        const resData = await fetch('https://cepatsehats.com/api/v3/cs/getKur.php', {
+        const formattedStartDate = startDate !== "" ? format(startDate, 'yyyy-MM-dd') : "";
+        const formattedEndDate = endDate !== "" ? format(endDate, 'yyyy-MM-dd') : "";
+
+        const url = `https://cepatsehats.com/api/v3/cs/getKur.php?startDate=${formattedStartDate}&endDate=${formattedEndDate}`;
+
+        const resData = await fetch(url, {
           method: 'GET',
           headers: {
             "Content-Type": "application/json",
-          },
+          }
         });
-
+        
         const result = await resData.json();
 
         if (result.status === 'success') {
-
-          const updatedData = result.data.map((item, index) => ({
-            ...item,
-            id: (index + 1).toString(),
-          }));
+          const updatedData = result.data.map((item, index) => {
+            const dateParts = item.created_at.split(' ')[0];
+            return {
+                ...item,
+                id: (index + 1).toString(),
+                created_at: dateParts,
+                updated_at: null
+            };
+          });
 
           setData(updatedData);
           setFilteredData(updatedData);
@@ -87,12 +101,23 @@ const dataKurBni = () => {
   }, []); 
 
   const handleFilter = (event) => {
-    const value = event.target.value.toLowerCase();
-    const filteredData = data.filter(row =>
-      row.name.toLowerCase().includes(value) ||
-      row.clinic_name.toLowerCase().includes(value) ||
-      row.clinic_address.toLowerCase().includes(value) 
-    );
+
+    if (!event || !event.target) {
+      return;
+    }
+  
+    const value = (event.target.value || '').toLowerCase();
+    const filteredData = data.filter(row => {
+      const rowDate = new Date(row.created_at);
+      return (
+        (!startDate || rowDate >= startDate) &&
+        (!endDate || rowDate <= endDate) &&
+        (row.name.toLowerCase().includes(value) ||
+        row.clinic_name.toLowerCase().includes(value) ||
+        row.clinic_address.toLowerCase().includes(value))
+      );
+    });
+
     setFilteredData(filteredData);
   };
 
@@ -113,14 +138,39 @@ const dataKurBni = () => {
       <link rel="stylesheet" href="assets/css/pbs/style.css" />
     </head>
     <div>
-    <div className="row mt-4">
-      <div className="col-7 col-lg-3 mx-3">
-        <input className="form-control" type="text" placeholder="Search" onChange={handleFilter} />
+    <div className="row mt-4 pb-3">
+      <div className="col-7 col-lg-3 mx-3 pt-3">
+        <input className="form-control" type="text" placeholder="Search" onChange={(event) => handleFilter(event)} />
       </div>
-      <div className="col-3">
-        <a className="btn btn-primary btn-sm" onClick={exportToExcel}>
+      <div className="col-3 col-lg-1 pt-3">
+        <a className="btn btn-primary btn-sm w-100" onClick={exportToExcel}>
           Export
         </a>
+      </div>
+      <div className='col-lg-2 col-5 mx-3 pt-3'>
+        <div className='row'>
+          <DatePicker
+            className='form-control'
+            selected={startDate}
+            onChange={date => setStartDate(date)}
+            dateFormat="yyyy-MM-dd"
+            placeholderText='Start Date'
+          />
+        </div>
+      </div>
+      <div className='col-lg-2 col-5 pt-3'>
+        <div className='row'>
+          <DatePicker
+            className='form-control'
+            selected={endDate}
+            onChange={date => setEndDate(date)}
+            dateFormat="yyyy-MM-dd"
+            placeholderText='end Date'
+          />
+        </div>
+      </div>
+      <div className='col-6 col-lg-2 mx-3 pt-3'>
+        <button className='btn btn-secondary' onClick={handleFilter}>Apply Filter</button>
       </div>
     </div>
     {loading ? (
