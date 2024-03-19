bc27673055b7a006fab8902a3ca1205b996beaba
erwin dwi s <erwin@waki.mobi>
2024-03-19 16:12:48 +0700

    modified form new article

--- src/app/admin/create-article/page.jsx
+++ src/app/admin/create-article/page.jsx
@@ -1,185 +1,262 @@
 "use client";
 
-import { Form, Container, Image } from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import React from "react";
-import { useState, useEffect } from "react";
-import { Modal, Button, Spinner } from "react-bootstrap";
-import axios from "axios";
-import ReactQuill from "react-quill";
+import { useState } from "react";
 import "react-quill/dist/quill.snow.css";
-import Cookies from "js-cookie";
 import NavbarAdmin from "../components/navbar";
-import { useRouter } from "next/navigation";
+
+import dynamic from 'next/dynamic';
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const QuillEditor = dynamic(() => import('react-quill'), { ssr: false });
 
 const Page = () => {
-  const [preview, setPreview] = useState(null);
+  
   const [isLoading, setIsLoading] = useState(false);
-  const [value, setValue] = useState("");
-  const router = useRouter();
+  const [imgBanner, setImgBanner] = useState();
+  const [valueTextEditor, setTextEditor] = useState();
 
-  const [form, setForm] = useState({
-    title: "",
-    description: "",
-    image: "",
+  const intialvalues = {
+    article_title: "",
+    article_category: "",
+    alt_img: "",
+    date_publish: "",
+    source: "",
+    is_active: false,
+    intro: "",
+  };
+
+  const validationsSchema = yup.object().shape({
+    article_title: yup.string().min(5).required('Required'),
+    article_category: yup.string().required("Required"),
+    alt_img: yup.string().required("Required"),
+    date_publish: yup.string().required("Required"),
+    is_active: yup.boolean(),
+    intro: yup.string().min(10).required('Required'),
   });
 
-  const handleChange = (e) => {
-    const { name, type, value } = e.target;
-    setForm({
-      ...form,
-      [name]: type === "file" ? e.target.files : e.target.value,
+  const onSubmit = async (values) => {
+    const { 
+      article_title, 
+      article_category, 
+      alt_img, 
+      date_publish, 
+      source, 
+      is_active, 
+      intro 
+    } = values;
+
+    const bodyJson = JSON.stringify({
+      title: article_title,
+      category: article_category,
+      alt_img: alt_img,
+      date_publish: date_publish,
+      source: source,
+      is_active: is_active,
+      intro: intro,
+      img_banner: imgBanner,
+      contents: valueTextEditor,
     });
-    if (e.target.type === "file") {
-      let url = URL.createObjectURL(e.target.files[0]);
-      setPreview(url);
-    }
-    console.log(form);
-  };
 
-  useEffect(() => {}, []);
-
-  const handlerSubmit = (e) => {
-    e.preventDefault();
-
-    const token = Cookies.get("islogin");
-    if (!token) {
-      alert("token not found");
-      router.push("/admin");
-      return;
-    }
-    const formData = new FormData();
-    formData.set("title", form.title);
-    formData.set("desc", value);
-    formData.set("image", form.image[0]);
-    const config = {
+    const postArticle = await fetch('/api/post-article', {
+      method: 'POST',
       headers: {
-        "Content-type": "multipart/form-data",
-        Authorization: "Bearer" + " " + token,
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin' : '*'
       },
-    };
+      body: bodyJson
+    });
 
-    axios
-      .post("http://localhost:5500/api/v1/article", formData, config)
-      .then(function (response) {
-        console.log(response);
-        alert("created article success");
-        setForm({
-          title: "",
-          description: "",
-          image: [],
-        });
-        setValue("");
-        router.push("/admin/dashboard");
-      })
-      .catch(function (error) {
-        console.log(error);
-        alert("created article failed");
-        setForm({
-          title: "",
-          description: "",
-          image: [],
-        });
-        setValue("");
-        router.push("/admin/dashboard");
-      });
+  }
+
+  const {values, errors, touched, handleSubmit, handleChange, handleBlur} = useFormik({
+    initialValues: intialvalues,
+    validationSchema: validationsSchema,
+    onSubmit,
+  });
+
+  const handleFileChange = (ev, setter) => {
+    const file = ev.target.files && ev.target.files[0];
+    if (file) {
+      if (file.size > 10 * 1024 * 1024) {
+        console.log('Too big brother')
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        setter(reader.result)
+        const imgPreview = document.getElementById('banner-priview');
+        imgPreview.src = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  const modules = {
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { header: [3, 4, 5, 6] }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ align: [] }],
+      [{ color: [] }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "image", "video"],
+      ["clean"],
+      ["code-block"],
+    ]
   };
+
+  const formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'align',
+    'color',
+    'list',
+    'link',
+    'image',
+    'video',
+    'code-block'
+  ];
 
   return (
     <>
       <NavbarAdmin />
-      <Container
-        fluid
-        className="d-flex justify-content-center text-black"
-        style={{ overflow: "auto", color: "black" }}
-      >
-        <Container className="px-5" style={{ height: "100vh" }}>
-          <Form onSubmit={(e) => handlerSubmit(e)}>
-            <h1 className="fs-3">Create Article</h1>
-            <Container className="d-flex gap-3 overflow-hidden">
-              <div className="gap-2 w-75">
-                <Form.Group className="">
-                  <Form.Label className="">Title Article</Form.Label>
-                  <Form.Control
-                    type="text"
-                    required
-                    onChange={handleChange}
-                    placeholder="Title"
-                    name="title"
-                    value={form.title}
-                    style={{
-                      backgroundColor: "rgba(210, 210, 210, 0.25)",
-                      border: "solid 2px #D2D2D2",
-                    }}
-                  />
-                </Form.Group>
-                <Form.Group controlId="formFile">
-                  <Form.Label className="">Image Article</Form.Label>
-                  <Form.Control
-                    type="file"
-                    required
-                    style={{
-                      backgroundColor: "rgba(210, 210, 210, 0.25)",
-                      border: "solid 2px #D2D2D2",
-                    }}
-                    name="image"
-                    onChange={handleChange}
-                  />
-                </Form.Group>
+      <Container>
+        <div style={{ marginTop: "0.5rem", display: "flex" }}>
+          <div className="col-lg-4 pb-4">
+            <h3>New Article</h3>
+            <form onSubmit={handleSubmit} autoComplete="off">
+              <div className="mb-2">
+                <label for="article_title" class="form-label">Title</label>
+                <input 
+                  type="text"
+                  id="article_title"
+                  value={values.article_title}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={`form-control ${errors.article_title && touched.article_title ? "is-invalid" : ""}`}
+                  required
+                />
+                {errors.article_title && touched.article_title && <p style={{ color: "red" }}>{errors.article_title}</p>}
               </div>
-              <div
-                className="w-25 bg-black"
-                style={{ position: "relative", overflow: "hidden" }}
-              >
-                <Image
-                  src={preview}
-                  rounded
-                  style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
-                  }}
+              <div className="mb-2">
+                <label for="article_category" className="form-label">Category</label>
+                <select 
+                  id="article_category"
+                  value={values.article_category}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={`form-control ${errors.article_category && touched.article_category ? "is-invalid" : ""}`}
+                  required
+                >
+                  <option value="" disabled>Choose Category</option>
+                  <option value="Kesehatan">Kesehatan</option>
+                  <option values="Makanan">Makanan</option>
+                </select>
+                {errors.article_category && touched.article_category && <p style={{ color: "red" }}>{errors.article_category}</p>}
+              </div>
+              <div className="mb-2">
+                <label for="img_banner" class="form-label">Cover Banner</label>
+                <img id="banner-priview" className="img-fluid" style={{ display: imgBanner ? 'block' : 'none' }} height="auto" width="450px" alt="Preview" /><br/>
+                <input
+                  type="file"
+                  id="img_banner"
+                  accept="image/*"
+                  onChange={(e) => handleFileChange(e, setImgBanner)}
+                  className={`form-control ${errors.img_banner && touched.img_banner ? "is-invalid" : ""}`}
+                  required
                 />
               </div>
-            </Container>
-
-            <Form.Group className="my-3" controlId="formBasicFullName">
-              <Form.Label className="">Desc Article</Form.Label>
-
-              <ReactQuill theme="snow" value={value} onChange={setValue} />
-            </Form.Group>
-
-            <Container className=" d-flex justify-content-end mt-3">
-              <Button
-                style={{ backgroundColor: "green" }}
-                variant="success"
-                size="lg"
-                disabled={isLoading}
-                type="submit"
-              >
-                create article
-              </Button>
-            </Container>
-          </Form>
-        </Container>
-        <Modal
-          show={isLoading}
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-        >
-          <Button disabled>
-            <Spinner
-              as="span"
-              animation="grow"
-              size="sm"
-              role="status"
-              aria-hidden="true"
-            />
-            Loading...
-          </Button>
-        </Modal>
+              <div className="mb-2">
+                <label for="alt_img" class="form-label">Alt Image Banner</label>
+                <input type="text"
+                  id="alt_img"
+                  value={values.alt_img}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={`form-control ${errors.alt_img && touched.alt_img ? "is-invalid" : ""}`}
+                  required
+                />
+                {errors.alt_img && touched.alt_img && <p style={{ color: "red" }}>{errors.alt_img}</p>}
+              </div>
+              <div className="mb-2">
+                <label for="source" class="form-label">Source</label>
+                <input type="text"
+                  id="source"
+                  value={values.source}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={`form-control ${errors.source && touched.source ? "is-invalid" : ""}`}
+                  required
+                />
+                {errors.source && touched.source && <p style={{ color: "red" }}>{errors.source}</p>}
+              </div>
+              <div className="mb-2">
+                <label for="date_publish" class="form-label">Publish Date</label>
+                <input 
+                  type="date" 
+                  id="date_publish" 
+                  name="date_publish"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={`form-control ${errors.date_publish && touched.date_publish ? "is-invalid" : ""}`}
+                  style={{ width: "100%" }}
+                  required
+                />
+                {errors.date_publish && touched.date_publish && <p style={{ color: "red" }}>{errors.date_publish}</p>}
+              </div>
+              <div className="mb-2">
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" 
+                    type="checkbox"              
+                    id="is_active"
+                    name="is_active"
+                    checked={values.is_active}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    />
+                  <label class="form-check-label" for="inlineCheckbox1">Active</label>
+                </div>
+              </div>
+              <div className="mb-2">
+                <label for="intro" class="form-label">Intro</label>
+                <textarea 
+                  id="intro" 
+                  rows="5" 
+                  cols="33"
+                  value={values.intro}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={`form-control ${errors.intro && touched.intro ? "is-invalid" : ""}`}
+                  required
+                >
+                </textarea>
+                {errors.intro && touched.intro && <p style={{ color: "red" }}>{errors.intro}</p>}
+              </div>
+              <button type="submit">Upload Article</button>
+            </form>
+          </div>
+          <div className="col-lg-8">
+            <div style={{ paddingLeft: "30px", marginTop: "70px" }}>
+              <QuillEditor 
+                theme="snow"
+                modules={modules}
+                formats={formats}
+                value={valueTextEditor} 
+                onChange={setTextEditor} 
+              />
+            </div>
+          </div>
+        </div>
       </Container>
     </>
   );
