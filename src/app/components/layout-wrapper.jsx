69b9593792ff3f6180994695c90e5bfbb19d4296
achyarrbagus <achyarbagus@gmail.com>
2024-03-21 04:22:01 +0700

    update param home back

--- src/app/components/layout-wrapper.jsx
+++ src/app/components/layout-wrapper.jsx
@@ -89,7 +89,7 @@ export default function LayoutWrapper({ children }) {
         <nav className="nav nav-top fixed-top">
           <div className="container">
             <div className="d-flex align-items-center gap-3">
-              <Link href={`/?${lang === "EN" ? "locale=en" : "locale=id"}`}>
+              <Link href={`/?${lang == "EN" ? "locale=en" : "locale=id"}`}>
                 <div style={{ cursor: "pointer" }} className="nav-brand">
                   <img src="/assets/img/logo.png" alt="" />
                 </div>
