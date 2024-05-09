dfac52ca1bdd7cd043374e1d6fc73b0181607dd6
achyarrbagus <achyarbagus@gmail.com>
2024-05-09 17:56:42 +0700

    update: redirect when user come from cept-sehat.com tocepatsehat.com

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
 import { redirect } from "next/navigation";
 
 export default function RootPage() {
+  if (window.location.hostname === "https://www.cepat-sehat.com/") {
+    window.location.replace("https://www.cepatsehat.com/");
+  }
+
+  console.log(window.location.hostname);
   redirect("/en");
 }
