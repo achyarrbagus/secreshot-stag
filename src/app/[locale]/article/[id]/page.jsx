033904228069c1f00c558390aac40db52e15b48c
achyarrbagus <achyarbagus@gmail.com>
2024-05-14 19:22:38 +0700

    update:-

--- src/app/[locale]/article/[id]/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Page({ params }) {
-  const { id } = params;
-  return (
-    <>
-      <h1>Hello world {id}</h1>
-    </>
-  );
-}
--- src/app/[locale]/article/[id]/staticParams.js
+++
@@ -1,3 +0,0 @@
-export function generateStaticParams() {
-  return [{ slug: ["a", "1"] }, { slug: ["b", "2"] }, { slug: ["c", "3"] }];
-}
